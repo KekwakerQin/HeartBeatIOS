15e88cbed1cc2365780ea28306703b4c2b2a5209
Chingis <Kekwaker@gmail.com>
2025-01-19 16:34:44 +0800

    Initial Commit

---
+++ HeartBeat/HeartBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartBeatApp.swift
+//  HeartBeat
+//
+//  Created by Qin Chingis on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HeartBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
